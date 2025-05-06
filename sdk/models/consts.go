f3e52e8840e5cfe0d2a2e3543a7bb9c2d6095165
Erez Levi <erez@prodstack.dev>
2025-05-06 08:34:11 +0000

    add general filters

--- sdk/models/consts.go
+++ sdk/models/consts.go
@@ -5,3 +5,12 @@ const (
 	ColumnOriginRoot = "root"
 	OpEqual          = "eq"
 )
+
+// Filter keys
+const (
+	Workload  = "workload"
+	Cluster   = "cluster"
+	Env       = "env"
+	Namespace = "namespace"
+	Instance  = "instance"
+)
